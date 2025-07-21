68adca0750b63e04b2ed1225c0576a97dae96e48
Laura Morales <moralesromanlaura@gmail.com>
2025-07-21 21:06:00 +0200

    I'm retarded

--- lua/functions.lua
+++ lua/functions.lua
@@ -32,8 +32,16 @@ vim.api.nvim_create_user_command('ToggleInlayHints', function()
     vim.lsp.inlay_hint.enable(inlay_hints_enabled)
 end, { nargs = 0 })
 
-vim.api.nvim_create_user_command('Title', 'norm i=<Esc>vy77po', { nargs = 0 })
-vim.api.nvim_create_user_command('Subtitle', 'norm i-<Esc>vy77po', { nargs = 0 })
+vim.api.nvim_create_user_command('Title', function()
+    vim.cmd('norm! i=')
+    vim.cmd('norm! vy77po')
+    vim.fn.feedkeys('i', 'n')
+end, { nargs = 0 })
+vim.api.nvim_create_user_command('Subtitle', function()
+    vim.cmd('norm! i-')
+    vim.cmd('norm! vy77po')
+    vim.fn.feedkeys('i', 'n')
+end, { nargs = 0 })
 
 -- in your init.lua:
 vim.api.nvim_create_user_command('ToggleEditorGuides', function()
