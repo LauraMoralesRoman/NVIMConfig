92b7035ed67f923ce5d61d56c818e460268bd398
Laura Morales Román <moralesromanlaura@gmail.com>
2026-01-26 14:54:11 +0100

    Added oil plugin

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -76,4 +76,29 @@ formatters_by_ft = {
     event = 'InsertEnter',
     config = true,
   },
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {
+            float = {
+                padding = 2,
+                max_width = 0.4,
+                max_height = 0.4,
+                border = 'rounded'
+            },
+            keymaps = {
+                ["q"] = "actions.close", -- press q to close the Oil buffer :contentReference[oaicite:2]{index=2}
+            },
+        },
+        -- Optional dependencies
+        dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        keys = {
+            { '-',         '<cmd>Oil --float<cr>',   desc = 'Shows file explorer' },
+            { '<Leader>-', '<cmd>Oil --float .<cr>', desc = 'Shows file explorer at the root directory' }
+        }
+    }
 }
