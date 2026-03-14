908374c543d755b0fb21a6f6f2e3abc32ba89fe5
Laura Morales Román <moralesromanlaura@gmail.com>
2026-03-14 17:26:01 +0100

    Added command to follow the qflist with the cursor

--- lua/commands.lua
+++ lua/commands.lua
@@ -51,3 +51,95 @@ vim.api.nvim_create_user_command('Here', function()
   local dir = vim.fn.expand('%:p:h')
   vim.cmd('cd ' .. dir)
 end, { desc = 'Set global working directory to current buffer directory' })
+
+-- Automatic qflist and loclist navigation
+
+local qf_follow_enabled = false
+local qf_follow_group = vim.api.nvim_create_augroup("QfFollow", { clear = true })
+
+local qf_follow_ns = vim.api.nvim_create_namespace("QfFollowHL")
+
+local function qf_follow_attach()
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    group = qf_follow_group,
+    buffer = 0,
+    callback = function()
+      local qf_win = vim.api.nvim_get_current_win()
+      local info = vim.fn.getwininfo(qf_win)[1]
+      if info.quickfix == 1 then
+        if info.loclist == 1 then
+          pcall(vim.cmd, ":.ll")
+        else
+          pcall(vim.cmd, ":.cc")
+        end
+
+        -- highlight the line in the source buffer
+        local src_win = vim.api.nvim_get_current_win()
+        local src_buf = vim.api.nvim_get_current_buf()
+        local src_line = vim.api.nvim_win_get_cursor(src_win)[1] - 1 -- 0-indexed
+
+        vim.api.nvim_buf_clear_namespace(src_buf, qf_follow_ns, 0, -1)
+        vim.api.nvim_buf_set_extmark(src_buf, qf_follow_ns, src_line, 0, {
+          line_hl_group = "CurSearch", -- swap for any highlight group you prefer
+        })
+
+        -- return focus to the qf/loc window
+        vim.api.nvim_set_current_win(qf_win)
+      end
+    end,
+  })
+end
+
+local function toggle_qf_follow()
+  qf_follow_enabled = not qf_follow_enabled
+  vim.api.nvim_clear_autocmds({ group = qf_follow_group })
+
+  if qf_follow_enabled then
+    -- attach to qf/loc windows that are already open
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      group = qf_follow_group,
+      pattern = "quickfix",
+      callback = qf_follow_attach,
+    })
+    -- also attach immediately if we're already inside a qf window
+    local info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+    if info and info.quickfix == 1 then
+      qf_follow_attach()
+    end
+    vim.notify("QF follow: ON", vim.log.levels.INFO)
+  else
+    vim.notify("QF follow: OFF", vim.log.levels.INFO)
+  end
+end
+
+vim.api.nvim_create_user_command("QfFollowEnable", function()
+  qf_follow_enabled = true
+  vim.api.nvim_clear_autocmds({ group = qf_follow_group })
+
+  vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = qf_follow_group,
+    pattern = "quickfix",
+    callback = qf_follow_attach,
+  })
+
+  local info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+  if info and info.quickfix == 1 then
+    qf_follow_attach()
+  end
+
+  vim.notify("QF follow: ON", vim.log.levels.INFO)
+end, { desc = "Enable quickfix/loclist cursor follow" })
+
+vim.api.nvim_create_user_command("QfFollowDisable", function()
+  qf_follow_enabled = false
+  vim.api.nvim_clear_autocmds({ group = qf_follow_group })
+  vim.notify("QF follow: OFF", vim.log.levels.INFO)
+end, { desc = "Disable quickfix/loclist cursor follow" })
+
+vim.api.nvim_create_user_command("QfFollowToggle", function()
+  if qf_follow_enabled then
+    vim.cmd("QfFollowDisable")
+  else
+    vim.cmd("QfFollowEnable")
+  end
+end, { desc = "Toggle quickfix/loclist cursor follow" })
--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -97,5 +97,5 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	  },
 	  opts = {}
-	}
+	},
 }
